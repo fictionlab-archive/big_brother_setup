e9db3d3c8f5986c95aba688d36d1a882bf15fa3d
Piotr <szlachcic49er@gmail.com>
2020-08-20 17:49:23 +0200

    Setup big_brother system

---
+++ files/setup.bash
@@ -0,0 +1,16 @@
+source /etc/ros/catkin_ws/devel/setup.bash
+
+### leo service variables
+
+# Path to the launch file to start
+export LAUNCH_FILE="/etc/ros/robot.launch"
+# Additional command-line arguments passed to roslaunch
+#export ROSLAUNCH_ARGS="--wait"
+
+
+# export ROS_HOME="/var/ros"
+# export ROS_HOSTNAME="master.localnet"
+export ROS_MASTER_URI="http://master.localnet:11311"
+export ROS_IP="10.0.0.100" 
+# ustawić statyczne ip
+#export ROS_NAMESPACE="leo1"
---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+rm -rf /etc/ros/catkin_ws
+mkdir -p -m 775 "/etc/ros/catkin_ws/src"
+
+
+pushd /etc/ros/catkin_ws/src
+git clone https://github.com/fictionlab/big_brother.git
+git clone https://github.com/fictionlab/ipcamera_driver.git
+cd ..
+catkin config --extend /opt/ros/melodic 
+rosdep update
+apt update
+rosdep install --rosdistro melodic --from-paths src -iy --os=ubuntu:bionic
+catkin build
+
+popd
+
+install -v -m 664 files/setup.bash "/etc/ros/"
+install -v -m 664 files/robot.launch "/etc/ros/"
+install -v -m 755 files/leo-start "/usr/bin/"
+install -v -m 755 files/leo-stop "/usr/bin/"
+install -v -m 644 files/leo.service "/etc/systemd/system/"
+
+systemctl enable leo
+
