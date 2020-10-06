b27a1937fbebc620101bd9567acd9b44d42cb355
szlachcic <szlachcic49er@gmail.com>
2020-10-06 12:09:08 +0000

    fix launch file name and command queue

--- files/setup.bash
+++ files/setup.bash
@@ -10,6 +10,9 @@ export LAUNCH_FILE="/etc/ros/system.launch"
 
 # export ROS_HOME="/var/ros"
 # export ROS_HOSTNAME="master.localnet"
+# export ROS_NAMESPACE="leo"
+
+#export master IP (LEO master IP: 10.0.0.1)
 export ROS_MASTER_URI="http://master.localnet:11311"
-export ROS_IP="10.0.0.100" 
-#export ROS_NAMESPACE="leo1"
+#export local IP (use ifconfig to investigate local IP)
+export ROS_IP="10.0.0.100"
--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ rm -rf /etc/ros/catkin_ws
 mkdir -p -m 775 "/etc/ros/catkin_ws/src"
 
 
-pushd /etc/ros/catkin_ws/src
+#pushd /etc/ros/catkin_ws/src
 # git clone https://github.com/fictionlab/big_brother.git
 # git clone https://github.com/fictionlab/ipcamera_driver.git
-install -v -m 664 packages/ipcamera_driver/* "/etc/ros/catkin_ws/src"
-install -v -m 664 packages/leo_big_brother/* "/etc/ros/catkin_ws/src"
-
+cp -r packages/ipcamera_driver /etc/ros/catkin_ws/src
+cp -r packages/leo_big_brother /etc/ros/catkin_ws/src
+pushd /etc/ros/catkin_ws/src
 cd ..
 catkin config --extend /opt/ros/melodic 
 rosdep update
@@ -19,7 +19,7 @@ catkin build
 popd
 
 install -v -m 664 files/setup.bash "/etc/ros/"
-install -v -m 664 files/robot.launch "/etc/ros/"
+install -v -m 664 files/system.launch "/etc/ros/"
 install -v -m 755 files/leo-start "/usr/bin/"
 install -v -m 755 files/leo-stop "/usr/bin/"
 install -v -m 644 files/leo.service "/etc/systemd/system/"
